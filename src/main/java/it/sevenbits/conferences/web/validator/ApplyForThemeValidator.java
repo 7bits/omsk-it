39734c8934afeb2f642307c481195c0dfe78bfe3
alex <alex@SamuraiU.(none)>
2013-11-06 23:42:20 +0700

    apply-for-theme validation and ajax

---
+++ src/main/java/it/sevenbits/conferences/web/validator/ApplyForThemeValidator.java
@@ -0,0 +1,11 @@
+package it.sevenbits.conferences.web.validator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alex
+ * Date: 06.11.13
+ * Time: 23:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class ApplyForThemeValidator {
+}
