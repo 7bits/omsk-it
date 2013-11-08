cdd316905fe342773209d32845d6859beb9226d3
simeon-slautin <simeon.o.slautin@gmail.com>
2013-11-08 17:13:07 +0700

    bash script for deploy on testing server

---
+++ deploy-testing.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# you need first generate a rsa keypair for ssh connect without password:
+# ssh-keygen
+# ssh-copy-id hostname
+# ssh hostname
+
+echo "Deleting old files from server..."
+ssh root@192.168.1.228 '
+    echo "stop tomcat";
+    /etc/init.d/tomcat7 stop;
+    echo "remove omsk-it.war";
+    rm omsk-it.war;
+    echo "remove $CATALINA_HOME/webapps/ROOT";
+    rm -r $CATALINA_HOME/webapps/ROOT;
+    echo "remove $CATALINA_HOME/webapps/ROOT.war";
+    rm $CATALINA_HOME/webapps/ROOT.war;
+'
+
+echo "Create war..."
+mvn clean package
+
+echo "Copy war to the testing server..."
+scp target/omsk-it.war root@192.168.1.228:/root
+
+echo "Deploy war to the CATALINA_HOME/webapps/ROOT.war..."
+ssh root@192.168.1.228 '
+    echo "copy war";
+    cp omsk-it.war $CATALINA_HOME/webapps/ROOT.war;
+    echo "start tomcat";
+    /etc/init.d/tomcat7 start;
+'
+
