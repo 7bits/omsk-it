4281e4970b971a67747fbb03a82cc6f0a3882310
alex499 <alex.zx2@yandex.ru>
2013-12-17 14:44:02 +0700

    upgrade rails_admin

--- rails-admin/app/models/conference.rb
+++ rails-admin/app/models/conference.rb
@@ -1,5 +1,43 @@
+# coding: utf-8
 class Conference < ActiveRecord::Base
   self.table_name = 'conference'
   belongs_to :company
   has_many :report
+
+  rails_admin do
+    label "Субботник"
+    label_plural "СУбботники"
+    object_label_method :ordinal_number
+
+    list do
+      field :id
+      field :ordinal_number do
+        label "Номер"
+      end
+      field :company do
+        label "Место проведения"
+      end
+      field :date do
+        label "Дата проведения"
+      end
+      field :registration do
+        label "Регистрация"
+      end
+    end
+
+    edit do
+      field :ordinal_number do
+        label "Номер"
+      end
+      field :company do
+        label "Место проведения"
+      end
+      field :date do
+        label "Дата проведения"
+      end
+      field :registration do
+        label "Регистрация"
+      end
+    end
+  end
 end
--- rails-admin/app/models/report.rb
+++ rails-admin/app/models/report.rb
@@ -1,5 +1,49 @@
+# coding: utf-8
 class Report < ActiveRecord::Base
   self.table_name = 'report'
   belongs_to :conference
   belongs_to :reporter
+
+  rails_admin do
+    label "Доклад"
+    label_plural "Доклады"
+    object_label_method :title
+
+    list do
+      field :id
+      field :title do
+        label "Название"
+      end
+      field :conference do
+        label "конференция"
+      end
+      field :reporter do
+        label "докладчик"
+      end
+    end
+
+    edit do
+      field :title do
+        label "Название"
+      end
+      field :conference do
+        label "конференция"
+      end
+      field :reporter do
+        label "докладчик"
+      end
+      field :description, :text do
+        label "описание"
+      end
+      field :presentation do
+        label "презентация"
+      end
+      field :video do
+        label "видео"
+      end
+      field :other_conferences
+      field :key_technologies
+      field :reporter_wishes
+    end
+  end
 end
