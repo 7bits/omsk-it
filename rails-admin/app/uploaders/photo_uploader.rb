f1710b147e44f975a06e41cbca366f3da00bb408
Sibert Dmitry <dimaaasik.s@gmail.com>
2014-02-12 19:11:46 +0700

    Fixed capistrano-deploy and image's dissapearing

--- rails-admin/app/uploaders/photo_uploader.rb
+++ rails-admin/app/uploaders/photo_uploader.rb
@@ -48,13 +48,13 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  def filename
-     #"something.jpg" if original_filename
-    if original_filename.nil?
-      "nophoto.png"
-    else
-      original_filename
-    end
-  end
+  #def filename
+  #   #"something.jpg" if original_filename
+  #  if original_filename.nil?
+  #    "nophoto.png"
+  #  else
+  #    original_filename
+  #  end
+  #end
 
 end
