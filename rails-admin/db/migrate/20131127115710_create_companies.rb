e3d8a818136ec00a29e4686c9fbde0a193169014
Sibert Dmitry <dimaaasik.s@gmail.com>
2014-02-13 11:09:44 +0700

    Removed unncessary files

--- rails-admin/db/migrate/20131127115710_create_companies.rb
+++
@@ -1,9 +0,0 @@
-class CreateCompanies < ActiveRecord::Migration
-  def down
-    create_table :company do |t|
-      t.string :name
-      t.string :site
-      t.string :location
-    end
-  end
-end
