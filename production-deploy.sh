21a5d49d505bcd989cf446908f3449e249dd6a1d
Sibert Dmitry <dimaaasik.s@gmail.com>
2014-03-11 11:10:23 +0700

    Added deploy script

---
+++ production-deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "Connecting to production server"
+ssh root@162.243.36.248 '
+    echo "Stoping tomcat";
+    /etc/init.d/tomcat7 stop;
+    cd /var/lib/tomcat7/webapps;
+    echo "Delete ROOT";
+    rm -r ROOT;
+    echo "Delete ROOT.war";
+    rm ROOT.war;
+'
+echo "Creting war"
+mvn clean package -P production
+echo "Copying to production"
+scp target/ROOT.war root@162.243.36.248:/var/lib/tomcat7/webapps
+echo "Restarting tomcat"
+ssh root@162.243.36.248 '
+    echo "Staring tomcat";
+    /etc/init.d/tomcat7 start;
+'
+echo "Deploying completed"
